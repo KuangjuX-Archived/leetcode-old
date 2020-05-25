62ce1d36e1fb121df801ee91914081be2aef7bcf
KuangjuX <18630816527@163.com>
2020-05-25 20:56:38 +0800

    reverse

--- Math/reverse.cpp
+++ Math/reverse.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 
 using namespace std;
+/*我的非常naive的做法*/
 class Solution {
 public:
     int reverse(int x) {
@@ -34,5 +35,25 @@ public:
         if(flag==0)x=-x;
 
         return x;
+    }
+};
+
+/*官方给的解答，利用弹出和推入数字&&溢出前检查*/
+/*
+ * 假设result为正数，当result>INT_MAX/10时，在此之后再乘10必然溢出
+ * 若result恰好等于INT_MAX/10，若pop大于7时也必将溢出
+ * 负数同理*/
+class Solution2 {
+public:
+    int reverse(int x) {
+        int result=0;
+        while(x!=0){
+            int pop=x%10;
+            x=x/10;
+            if(result>INT_MAX/10||((result==INT_MAX/10)&&pop>7))return 0;
+            if(result<INT_MIN/10||((result==INT_MIN/10)&&pop<-8))return 0;
+            result=result*10+pop;
+        }
+        return result;
     }
 };
