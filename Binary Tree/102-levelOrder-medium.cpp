5c47b90022feb15e3d588b5d6f6a534988d59ac2
KuangjuX <18630816527@163.com>
2020-05-27 00:03:03 +0800

    levelOrder

--- Binary Tree/102-levelOrder-medium.cpp
+++ Binary Tree/102-levelOrder-medium.cpp
@@ -2,6 +2,24 @@
 /*本题很好的解析的URL：https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/bfs-de-shi-yong-chang-jing-zong-jie-ceng-xu-bian-l/*/
 
 /**
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * 示例：
+   二叉树：[3,9,20,null,null,15,7],
+     3
+   / \
+  9  20
+    /  \
+   15   7
+
+   返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]*/
+
+/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
